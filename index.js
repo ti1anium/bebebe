834efd7dde599ccd3080c1537aa60824ce98268a
ti1anium <markhomepost@gmail.com>
2025-11-30 14:47:04 +0300

    js scripted

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
     const res = await fetch("./db.json");
     const data = await res.json();
 
-    console.log(data);
+    const cards = document.getElementsByClassName("cards")[0];
+
+    for (let card of data.product) {
+        const cardElement = document.createElement("div");
+
+        cardElement.innerHTML = `
+            <h1 class="name">${card.name}</h1>
+            <p class="price">${card.price}$</p>
+            <img src="${card.image}">
+            <p class="description">${card.description}</p>
+            <p class="color">Color: ${card.color}</p>
+        `;
+
+        cards.appendChild(cardElement);
+    }
 })();
